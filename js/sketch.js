a908d9db1d7c1eb5427b59ab501489059b5a0659
alexanddretgneto <alexanddretgneto@gmail.com>
2023-09-02 17:16:33 -0300

    modificação

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,9 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
     background(0);
   }
   
   function draw() {
     
-    circle(mouseX, mouseY, 6);
+    circle(mouseX, mouseY, 4);
   }
