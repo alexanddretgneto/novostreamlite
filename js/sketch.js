414a2c77b8a56eaf348500b05c92c28bf38271cb
alexanddretgneto <alexanddretgneto@gmail.com>
2023-09-02 23:53:48 -0300

    Enviando 2

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,9 @@
 function setup() {
-    createCanvas(600, 400);
-    background(0);
-  }
-  
-  function draw() {
-    
-    circle(mouseX, mouseY, 4);
-  }
+  createCanvas(600, 400);
+  background(0);
+}
+
+function draw() {
+
+  circle(mouseX, mouseY, 5);
+}
